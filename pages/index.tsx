a470bbe36947678efafe45adf63e9106615f629b
israelmbg <israelmbg3@gmail.com>
2020-11-06 12:43:00 -0300

    add typescript and eslint/prettier config

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bin2Dec</title>
+      </Head>
+
+      <main>
+        <h1>Binary to Decimal</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
